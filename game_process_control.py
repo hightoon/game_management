f094f4a758748dd69cc747dee2aae169332d944e
hightoon <haitong.chen@gmail.com>
2015-06-26 06:29:13 +0800

    update game controller, now support start/stop game from remote host

--- game_process_control.py
+++ game_process_control.py
@@ -5,7 +5,7 @@ import time
 from subprocess import Popen
 from bottle import route, request, get, post, run
 
-running_games = None
+running_game = None
 
 class Game(object):
     def __init__(self, path, name):
@@ -27,7 +27,7 @@ class Game(object):
         if self._running:
             return self._process.poll()
 
-@post('/start_game')
+@route('/start_game')
 def start_game():
     global running_game
     game = Game("D:\\GameManagememt\\[vrword.cn]³¬¼¶ÀºÇò\\Baskhead v0.1\\Baskhead", "BaskHead")
@@ -35,13 +35,15 @@ def start_game():
     if running_game:
         running_game.stop()
     running_game = game
+    return 'baskhead'
 
-@post('/stop_game')
+@route('/stop_game')
 def stop_game():
     global running_game
     if running_game:
         running_game.stop()
         running_game = None
+    return 'ok'
 
 def run_svr():
     run(host='192.168.0.4', port=8080, debug=True)

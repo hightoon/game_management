fbe5249c211a66f2c9c19972aa0d01671c97b35d
hightoon <haitong.chen@gmail.com>
2015-07-08 21:44:06 +0800

    render page with bottle simple template

--- app_mngr.py
+++ app_mngr.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*-
 """
   Simple web server for local application management, based on bottle framework
-
   Author: haitong.chen@gmail.com
 """
+
+import time
 import urllib2
 from subprocess import Popen
 from bottle import route, request, redirect, template,static_file, run
 
 apps = []
 user = None
+
+class User:
+  def __init__(self, usrname, password, nickname, description):
+    self._usrname = usrname
+    self._password = password
+    self._nickname = nickname
+    self._desc =description
+
+  def write2db(self):
+    # store user info to database
+    pass
 
 @route('/')
 def main():
@@ -23,38 +35,27 @@ def login():
     redirect('/static/management_front_end/admin_mngm/index.html')
   else:
     redirect('/static/management_front_end/login_page.html')
-  if 0:
-    return '''
-        <html>
-        <head>
-            <h3>登录</h3>
-            <style>
-                html,body{text-align:center;margin:100px auto;}
-            </style>
-        </head>
-        <body>
-        <form action="/login" method="post">
-            用户名: <input name="username" type="text" /><br/><br/>
-            密   码: <input name="password" type="password" />
-            <br/><input value="Login" type="submit" />
-        </form></body>
-        </html>
-    '''
 
 @route('/login', method='POST')
 def do_login():
   global user
   print "get login post"
+  forgot = None
   username = request.forms.get('username')
   password = request.forms.get('password')
+  forgot = request.forms.get('forget_passwd')
+  if forgot:
+    print forgot
+    print 'sending password to %s'%(username,)
+    return "密码发送至，请查看邮箱并返回登录页面重新登录。。。"
+    #redirect('/static/management_front_end/login_page.html')
   print username, password
   if check_login(username, password):
     user = username
-    if user == 'admin':
-      redirect('/static/management_front_end/admin_mngm/index.html')
+    redirect('/index/%s'%(user,))
+      #redirect('/static/management_front_end/admin_mngm/index.html')
   else:
-    #return "<p>Login failed.</p>"
-    redirect('/login') 
+    redirect('/login')
 
 def check_login(usr, pwd):
   if usr == 'admin' and pwd == 'admin':
@@ -68,6 +69,13 @@ def user_logout():
   user = None
   redirect('/login')
 
+@route('/index/<user>')
+def user_index(user=user):
+  if user == 'admin':
+    myGames = [('hostA', '192.168.0.1', 'basketball', 'running'),
+             ('hostA', '192.168.0.1', 'basketball', 'running'),]
+    return template('./management_front_end/admin_mngm/index', is_admin=True, games=myGames)
+
 @route('/static/<filename:path>')
 def send_static(filename):
   return static_file(filename, root='./')
@@ -75,6 +83,12 @@ def send_static(filename):
   #  redirect('/')
   #else:
   #  return template('./management_front_end/admin_mngm/index')
+
+@route('/test_temp')
+def test_temp():
+  myGames = [('hostA', '192.168.0.1', 'basketball', 'running'),
+           ('hostA', '192.168.0.1', 'basketball', 'running'),]
+  return template('./management_front_end/usr_mgr/test_temp.tpl', games=myGames)
 
 @route("/start_game", method="POST")
 def start_app():
@@ -93,4 +107,5 @@ def stop_app():
   redirect('/%s/index'%(user))
 
 
-run(host='0.0.0.0', port=80, debug=True)
+run(host='0.0.0.0', port=80, Debug=True)
+#run(host='localhost', port=80, Debug=True)
